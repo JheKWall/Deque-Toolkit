d741a2012a712a7149317eb89d9e03e27c298d09
WillHayes096283 <wilthaye@ut.utm.edu>
2022-11-22 11:16:05 -0600

    added makefile and main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author William Hayes, Jackson Horton, and Kenneth Wallace
+ * @date 2022-11-22
+ * @brief short
+ * 
+ * long
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  
+  return 0;
+}
