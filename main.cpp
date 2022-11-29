2d6145cb3e4a9f4603e27964a09670249bcf81d3
WillHayes096283 <wilthaye@ut.utm.edu>
2022-11-29 11:27:20 -0600

    added test cases

--- main.cpp
+++ main.cpp
@@ -15,10 +15,13 @@ using namespace std;
 
 int main() {
   deque<int> mydeque;
-
+  
+  
   if (mydeque.empty())
     cout << "The deque is empty!" << endl;
 
+  cout << "Pushing 12345 integers front and back..." << endl << endl;
+  
   for (int i = 0; i < 12345; i++) {
     // push elements to front and back
     if (i % 2)
@@ -27,8 +30,61 @@ int main() {
       mydeque.push_front(i);
   }
 
+  cout << "All push_front's and push_back's completed!" << endl;
+  cout << "Size of deque: " << mydeque.size() << endl << endl;
+  
+  cout << "Testing print method..." << endl << endl;
+  
   mydeque.print();
 
+  cout << endl;
+
+  cout << "Testing front() and back()..." << endl << endl;;
+
+  cout << "Front(): " << mydeque.front() << endl;
+  cout << "Back(): " << mydeque.back() << endl << endl;
+
+  cout << "Testing pop_front and pop_back..." << endl << endl;
+
+  mydeque.pop_front();
+  mydeque.pop_back();
+
+  cout << "Printing new front and back..." << endl << endl;
+  
+  cout << "New front(): " << mydeque.front() << endl;
+  cout << "New back(): " << mydeque.back() << endl << endl;
+
+  cout << "Popping all elements in array..." << endl;
+
+  for(int i = 0; i < mydeque.size(); i++) {
+    if(i % 2)
+      mydeque.pop_back(i);
+    else
+      mydeque.pop_front(i);
+  }
+
+  cout << "All elements popped!" << endl << endl;
+
+
+  cout << "Testing overloaded []'s..." << endl << endl;
+  cout << "Creating deque of 5 integers" << endl;
+
+  for(int i = 0; i < 5; i++){
+    mydeque.push_back(i);
+  }
+
+  cout << "Printing deque using []'s: " << endl;
+  for(int i = 0; i < mydeque.size(); i++) {
+    cout << "mydeque[i]: " << mydeque[i] << endl;
+  }
+
+  cout << "Modifying deque data using []'s:" << endl;
+  mydeque[0] = 42;
+  mydeque[3] = 7777;
+
+  for(int i = 0; i < mydeque.size(); i++) {
+    cout << "mydeque[i]: " << mydeque[i] << endl;
+  }
   
   return 0;
 }
